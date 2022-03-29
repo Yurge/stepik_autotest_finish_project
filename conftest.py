7056aa5a39fce0a4c7201f3d3eaec59050a62cf8
Yury <vibornih@bk.ru>
2022-03-29 23:54:52 +0300

    changed scope in @pytest.fixture(scope="module")

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en', help="Choose language")
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def browser(request):
     print("\nstart chrome browser for test..")
     lang = request.config.getoption("language")
