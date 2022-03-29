a864a750c9965744dc4b74d57a7e1ca61d8e355d
Yury <vibornih@bk.ru>
2022-03-30 00:00:46 +0300

    added def test_message_disappeared_after_adding_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -11,6 +11,34 @@ def test_guest_can_add_product_to_basket(browser, end):
     page.open()
     page.add_product_to_basket()
     page.matem()
-    page.added_product_in_basket()
+    page.should_be_success_message()
     page.name_book_in_message()
     page.price_book_in_message()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    browser.delete_all_cookies()
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    browser.delete_all_cookies()
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    browser.delete_all_cookies()
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_disappeared()
