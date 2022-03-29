2d6e44d1578d7050653122405687d56e8aa14183
Yury <vibornih@bk.ru>
2022-03-29 22:22:10 +0300

    added @pytest.mark.parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,12 @@
 from .pages.product_page import ProductPage
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('end', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
 
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+def test_guest_can_add_product_to_basket(browser, end):
+    browser.delete_all_cookies()
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{end}'
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
