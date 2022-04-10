df9a0c3d97ebd18e4553a5ebb0f70bfdc6a49301
Yury <vibornih@bk.ru>
2022-04-10 13:31:42 +0300

    добавлены методы: go_to_basket, go_to_login_page, is_disappeared, should_be_login_link, should_be_authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators, MainPageLocators, BasketPageLocators
 import math
 
 class BasePage():
@@ -11,6 +12,16 @@ class BasePage():
         self.browser = browser
         self.link = link
         self.browser.implicitly_wait(timeout)
+
+
+    def go_to_basket(self):
+        link_basket = self.browser.find_element(*MainPageLocators.BUTTON_BASKET)
+        link_basket.click()
+
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
 
     def open(self):
@@ -25,20 +36,20 @@ class BasePage():
         return True
 
 
-    def is_not_element_present(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True
-        return False
-
-
     def is_disappeared(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
         return True
+
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
 
 
     def solve_quiz_and_get_code(self):
@@ -54,3 +65,12 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user"
