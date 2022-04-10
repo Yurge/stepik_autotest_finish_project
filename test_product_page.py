3044aa1295ab8db8f9cb9e2cf354abd1a7437393
Yury <vibornih@bk.ru>
2022-04-10 13:42:18 +0300

    добавлены методы: test_guest_can_go_to_login_page_from_product_page, test_guest_should_see_login_link_on_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -42,3 +42,36 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.should_be_disappeared()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+#class TestUserAddToBasketFromProductPage():
+#
+#    def test_user_cant_see_success_message(browser):
+#        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+#        page = ProductPage(browser, link)
+#        page.open()
+#        page.should_not_be_success_message()
+#
+#    def test_user_can_add_product_to_basket(browser, end=9):
+#        link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{end}'
+#        page = ProductPage(browser, link)
+#        page.open()
+#        page.add_product_to_basket()
+#        page.matem()
+#        page.should_be_success_message()
+#        page.name_book_in_message()
+#        page.price_book_in_message()
