a36a9bdac3966d56db10e4679dc1feef17ce4cb8
Yury <vibornih@bk.ru>
2022-04-10 13:58:26 +0300

    изменен второй assert в def nothing_in_basket

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -5,4 +5,4 @@ class BasketPage(BasePage):
 
     def nothing_in_basket(self):
         assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), 'В корзине есть товар (товары)'
-        assert self.is_element_present(*BasketPageLocators.NOTHING_IN_BASKET), 'В корзине есть товар!'
+        assert self.browser.find_element(*BasketPageLocators.NOTHING_IN_BASKET).text, 'нет текста "корзина пуста"'
