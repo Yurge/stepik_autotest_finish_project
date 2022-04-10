7b95440dadbe9311ef112c15e6ed8e6db22a80e9
Yury <vibornih@bk.ru>
2022-04-10 14:00:09 +0300

    добавлен import ProductPage, добавлен метод test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
 import pytest, time
 
 
@@ -21,6 +22,14 @@ def test_guest_should_see_login_link(browser):
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page = BasketPage(browser, browser.current_url)
+    page.nothing_in_basket()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/hacking-exposed-wireless_208/'
+    page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
     page = BasketPage(browser, browser.current_url)
