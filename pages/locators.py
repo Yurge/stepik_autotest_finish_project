80c89cb4838fc0844f594870c1194ff7861665d2
Yury <vibornih@bk.ru>
2022-04-10 13:36:49 +0300

    добавлен class BasketPageLocators, обновлены классы: BasePageLocators и MainPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,26 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class BasketPageLocators():
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "#basket_formset")
+    NOTHING_IN_BASKET = (By.CSS_SELECTOR, "#content_inner p")
+
 
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, ".register_form")
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BUTTON_BASKET = (By.CSS_SELECTOR, ".basket-mini span a")
 
 
 class ProductPageLocators():
